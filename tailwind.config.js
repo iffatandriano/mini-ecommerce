42a9349025045caf311b646a62d11854b94ceb29
Iffat Andriano <iffatandriano@gmail.com>
2022-09-15 04:48:05 +0700

    fix eslint and mocks components product, category

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0.3rem 0.3rem 0 #222",
+      },
+    },
   },
   plugins: [],
 };
